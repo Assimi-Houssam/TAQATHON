170a85ca771357b5a731566115a3e125a483e7f0
Labrahmi <yousseflabrahmi8@gmail.com>
2025-07-09 18:58:24 +0100

    chore: remove outdated MOCK_API_README and update branding in localization files for TAQA

--- frontend/src/app/[locale]/metadata.ts
+++ frontend/src/app/[locale]/metadata.ts
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "OCP Achat",
-  description: "Web app for OCP Achat, created by 1337 Dev Team",
+  title: "TAQATHON",
+  description: "Web app for TAQATHON, created by 1337 Dev Team",
 }; 
